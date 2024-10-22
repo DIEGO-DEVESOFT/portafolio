35ed61eb74452293960d74e547da8218b103f399
camarav380pro <asoinco64@gmail.com>
2024-10-21 22:41:19 -0500

    portafolio

---
+++ script.js
@@ -0,0 +1,10 @@
+//icono para subir la pagina rapido 
+
+// Función para volver al principio de la página
+document.getElementById('scrollToTop').addEventListener('click', function (event) {
+    event.preventDefault(); // Evita que el enlace siga el enlace
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // Desplazamiento suave
+    });
+});
